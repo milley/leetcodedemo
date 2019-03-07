140a51cd0e77d5bab89beb5f6ac878d639e637d2
milley <milley.gm@gmail.com>
2019-03-07 15:16:34 +0800

    complete the binary search

---
+++ src/main/java/com/milley/structure/search/BinarySearch.java
@@ -0,0 +1,111 @@
+package com.milley.structure.search;
+
+public class BinarySearch {
+    public static int bsearch(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int bsearchWithRecursion(int[] a, int n, int value) {
+        return bsearchInternally(a, 0, n - 1, value);
+    }
+
+    private static int bsearchInternally(int[] a, int low, int high, int value) {
+        if (low > high) return -1;
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bsearchInternally(a, mid + 1, high, value);
+        } else {
+            return bsearchInternally(a, low, mid - 1, value);
+        }
+    }
+
+    public static int binarySearchFirstElement(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == 0) || a[mid - 1] != value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchLastElement(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if (mid == n - 1 || a[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchFirstGreaterThanElement(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if (mid == 0 || a[mid - 1] < value) {
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchLastLessThanElement(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if (mid == n - 1 || a[mid + 1] != value) {
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/com/milley/structure/search/BinarySearchTest.java
@@ -0,0 +1,84 @@
+package com.milley.structure.search;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearchTest {
+    private int[] a1;
+    private int[] a2;
+    private int[] a3;
+
+    @Before
+    public void init() {
+        a1 = new int[]{4, 5, 6, 11, 12, 23};
+        a2 = new int[]{4, 5, 5, 5, 12, 23};
+        a3 = new int[]{2, 5, 6, 6, 8, 9, 10};
+    }
+
+    @Test
+    public void bsearchTest() {
+        int index = BinarySearch.bsearch(a1, a1.length, 11);
+        assertEquals(3, index);
+        index = BinarySearch.bsearch(a1, a1.length, 4);
+        assertEquals(0, index);
+        index = BinarySearch.bsearch(a1, a1.length, 23);
+        assertEquals(5, index);
+        index = BinarySearch.bsearch(a1, a1.length, 7);
+        assertEquals(-1, index);
+
+        index = BinarySearch.bsearchWithRecursion(a1, a1.length, 11);
+        assertEquals(3, index);
+        index = BinarySearch.bsearchWithRecursion(a1, a1.length, 4);
+        assertEquals(0, index);
+        index = BinarySearch.bsearchWithRecursion(a1, a1.length, 23);
+        assertEquals(5, index);
+        index = BinarySearch.bsearchWithRecursion(a1, a1.length, 7);
+        assertEquals(-1, index);
+    }
+
+    @Test
+    public void binarySearchFirstElementTest() {
+        int index = BinarySearch.binarySearchFirstElement(a2, a2.length, 11);
+        assertEquals(-1, index);
+        index = BinarySearch.binarySearchFirstElement(a2, a2.length, 4);
+        assertEquals(0, index);
+        index = BinarySearch.binarySearchFirstElement(a2, a2.length, 23);
+        assertEquals(5, index);
+        index = BinarySearch.binarySearchFirstElement(a2, a2.length, 5);
+        assertEquals(1, index);
+    }
+
+    @Test
+    public void binarySearchLastElementTest() {
+        int index = BinarySearch.binarySearchLastElement(a2, a2.length, 11);
+        assertEquals(-1, index);
+        index = BinarySearch.binarySearchLastElement(a2, a2.length, 4);
+        assertEquals(0, index);
+        index = BinarySearch.binarySearchLastElement(a2, a2.length, 23);
+        assertEquals(5, index);
+        index = BinarySearch.binarySearchLastElement(a2, a2.length, 5);
+        assertEquals(3, index);
+    }
+
+    @Test
+    public void binarySearchFirstGreaterThanElementTest() {
+        int index = BinarySearch.binarySearchFirstGreaterThanElement(a3, a3.length, 7);
+        assertEquals(4, index);
+        index = BinarySearch.binarySearchFirstGreaterThanElement(a3, a3.length, 8);
+        assertEquals(4, index);
+        index = BinarySearch.binarySearchFirstGreaterThanElement(a3, a3.length, 6);
+        assertEquals(2, index);
+    }
+
+    @Test
+    public void binarySearchLastLessThanElementTest() {
+        int index = BinarySearch.binarySearchLastLessThanElement(a3, a3.length, 7);
+        assertEquals(3, index);
+        index = BinarySearch.binarySearchLastLessThanElement(a3, a3.length, 8);
+        assertEquals(4, index);
+        index = BinarySearch.binarySearchLastLessThanElement(a3, a3.length, 6);
+        assertEquals(3, index);
+    }
+}
