9d51aab02a71f92393e7001fa23ade553f40bfe2
milley <milley.gm@gmail.com>
2019-05-02 18:11:57 +0800

    top k another version solution

--- src/main/java/com/milley/TopKFrequentElements/TopKSolution.java
+++ src/main/java/com/milley/TopKFrequentElements/TopKSolution.java
@@ -35,9 +35,35 @@ public class TopKSolution {
         return elements;
     }
 
+    public static List<Integer> topKFrequentA(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // init heap 'the less frequent element first'
+        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        // keep k top frequent elements in the heap
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        // build output list
+        List<Integer> top_k = new LinkedList<>();
+        while (!heap.isEmpty()) {
+            top_k.add(heap.poll());
+        }
+        Collections.reverse(top_k);
+        return top_k;
+    }
+
     public static void main(String[] args) {
         //int[] nums = {1, 1, 1, 2, 2, 3};
         int[] nums = {4, 1, -1, 2, -1, 2, 3};
-        System.out.println(topKFrequent(nums, 2));
+        System.out.println(topKFrequentA(nums, 2));
     }
 }
