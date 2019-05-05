eac90a73c577e15ffaf119dedee52d422fcb9d38
milley <milley.gm@gmail.com>
2019-05-05 20:22:04 +0800

    commit Count And Say

---
+++ src/main/java/com/milley/CountAndSay/Solution.java
@@ -0,0 +1,37 @@
+package com.milley.CountAndSay;
+
+public class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        } else {
+            return build(countAndSay(n - 1));
+        }
+    }
+
+    private String build(String numStr) {
+        StringBuffer sb = new StringBuffer();
+        char previous = numStr.charAt(0);
+        int index = 1;
+
+        for (int i = 1; i < numStr.length(); i++) {
+            char current = numStr.charAt(i);
+            if (current == previous) {
+                index++;
+            } else {
+                sb.append(index);
+                sb.append(previous);
+                previous = current;
+                index = 1;
+            }
+        }
+        sb.append(index);
+        sb.append(previous);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.countAndSay(5));
+    }
+}
