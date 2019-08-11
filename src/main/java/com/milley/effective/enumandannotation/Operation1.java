01759979cc458268b1b0bf4307398431b65f3cba
milley <milley.gm@gmail.com>
2019-08-11 22:09:31 +0800

    enum tutorial

---
+++ src/main/java/com/milley/effective/enumandannotation/Operation1.java
@@ -0,0 +1,20 @@
+package com.milley.effective.enumandannotation;
+
+public enum Operation1 {
+    PLUS, MINUS, TIMES, DIVIDE;
+
+    // Do the arithemtic operation represented by this constant
+    public double apply(double x, double y) {
+        switch (this) {
+            case PLUS:
+                return x + y;
+            case MINUS:
+                return x - y;
+            case TIMES:
+                return x * y;
+            case DIVIDE:
+                return x / y;
+        }
+        throw new AssertionError("Unknown op: " + this);
+    }
+}
