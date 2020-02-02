2706ac6c4d64c9b3328c356858a8641ec18bb2c5
milley <milley.gm@gmail.com>
2020-02-03 00:23:24 +0800

    power of two

---
+++ src/main/java/com/milley/poweroftwo/Solution.java
@@ -0,0 +1,19 @@
+package com.milley.poweroftwo;
+
+/**
+ * @author milley
+ */
+public class Solution {
+    public static boolean isPowerOfTwo(int n) {
+        if ((n & (n - 1)) == 0 && (n != 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(isPowerOfTwo(218));
+    }
+}
