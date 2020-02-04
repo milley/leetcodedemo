e0ef409d81e5826ac74a2d7cd537fcbbc9bccf5e
milley <milley.gm@gmail.com>
2020-02-04 22:48:54 +0800

    NQueens with bits

--- src/main/java/com/milley/nqueensii/Solution.java
+++ src/main/java/com/milley/nqueensii/Solution.java
@@ -4,7 +4,7 @@ package com.milley.nqueensii;
  * @author milley
  */
 public class Solution {
-    int count = 0;
+    public static int count = 0;
     public int totalNQueens(int n) {
         boolean[] cols = new boolean[n];
         boolean[] d1 = new boolean[2 * n];
@@ -29,5 +29,34 @@ public class Solution {
                 cols[col] = false; d1[id1] = false; d2[id2] = false;
             }
         }
+    }
+
+    public int totalNQueensWithBit(int n) {
+        dfs(n, 0, 0, 0, 0);
+        return count;
+    }
+
+    private void dfs(int n, int row, int cols, int pie, int na) {
+        if (row >= n) {
+            count++;
+            return;
+        }
+
+        // 得到所有的空位
+        int bits = (~(cols | pie | na)) & ((1 << n) - 1);
+        while (bits > 0) {
+            // 取最低位的1
+            int p = bits & -bits;
+            dfs(n, row + 1, cols | p, (pie | p) << 1,
+                    (na | p) >> 1);
+            // 去掉最低位的1
+            bits = bits & (bits - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        // new Solution().totalNQueens(4);
+        new Solution().totalNQueensWithBit(4);
+        System.out.println(count);
     }
 }
