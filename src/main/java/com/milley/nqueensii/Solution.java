c282fbc66629cf56611096501789391f992ffbcf
milley <milley.gm@gmail.com>
2020-02-05 10:16:27 +0800

    NQueens with bits--print result

--- src/main/java/com/milley/nqueensii/Solution.java
+++ src/main/java/com/milley/nqueensii/Solution.java
@@ -1,10 +1,17 @@
 package com.milley.nqueensii;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 /**
  * @author milley
  */
 public class Solution {
-    public static int count = 0;
+    private static int count = 0;
+    /**
+     * add for print
+     */
+    private Set<Integer> currSet = new LinkedHashSet<>();
     public int totalNQueens(int n) {
         boolean[] cols = new boolean[n];
         boolean[] d1 = new boolean[2 * n];
@@ -15,6 +22,7 @@ public class Solution {
 
     private void helper(int row, boolean[] cols, boolean[] d1, boolean[] d2, int n) {
         if (row == n) {
+            System.out.println(currSet);
             count++;
         } else {
             for (int col = 0; col < n; col++) {
@@ -25,8 +33,10 @@ public class Solution {
                 }
 
                 cols[col] = true; d1[id1] = true; d2[id2] = true;
+                currSet.add(col);
                 helper(row + 1, cols, d1, d2, n);
                 cols[col] = false; d1[id1] = false; d2[id2] = false;
+                currSet.remove(col);
             }
         }
     }
@@ -36,9 +46,34 @@ public class Solution {
         return count;
     }
 
+    private static int getSeqOfOneFromLowEndian(int n) {
+        int seq = 0;
+        if (n == 0) {
+            return seq;
+        }
+        while (n > 0) {
+            if ((n & 1)  == 1) {
+                return seq;
+            } else {
+                seq++;
+                n = n >> 1;
+            }
+        }
+        return seq;
+    }
+
+    private void printNumbers(Set<Integer> set) {
+        for (Integer i : set) {
+            System.out.print(getSeqOfOneFromLowEndian(i.intValue())+ " ");
+        }
+        System.out.println();
+    }
+
     private void dfs(int n, int row, int cols, int pie, int na) {
         if (row >= n) {
             count++;
+            // System.out.println(currSet);
+            printNumbers(currSet);
             return;
         }
 
@@ -47,15 +82,17 @@ public class Solution {
         while (bits > 0) {
             // 取最低位的1
             int p = bits & -bits;
+            currSet.add(p);
             dfs(n, row + 1, cols | p, (pie | p) << 1,
                     (na | p) >> 1);
             // 去掉最低位的1
             bits = bits & (bits - 1);
+            currSet.remove(p);
         }
     }
 
     public static void main(String[] args) {
-        // new Solution().totalNQueens(4);
+        new Solution().totalNQueens(4);
         new Solution().totalNQueensWithBit(4);
         System.out.println(count);
     }
